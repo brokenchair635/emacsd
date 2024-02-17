40dbe39e9898d986b97a25d7aa075215385a5d60
systemadmin <systemadmin@MacBook-Air.local>
2024-02-17 14:19:25 +0900

    my_setting

---
+++ elpa/flycheck-20240212.756/flycheck-pkg.el
@@ -0,0 +1,16 @@
+(define-package "flycheck" "20240212.756" "On-the-fly syntax checking"
+  '((emacs "26.1")
+    (pkg-info "0.4")
+    (seq "2.24"))
+  :commit "2b0f85375c8af5b65187cd8aa3d6b286639b2c33" :authors
+  '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
+  :maintainers
+  '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com"))
+  :maintainer
+  '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+  :keywords
+  '("convenience" "languages" "tools")
+  :url "http://www.flycheck.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

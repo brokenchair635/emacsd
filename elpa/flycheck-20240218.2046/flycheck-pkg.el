0b2790abe9050c6676f89722ea93e888a4e2fc2f
brokenchair635 <kimminsik2006@icloud.com>
2024-02-19 13:48:59 +0900

    update

--- elpa/flycheck-20240218.2046/flycheck-pkg.el
+++ elpa/flycheck-20240218.2046/flycheck-pkg.el
@@ -1,8 +1,6 @@
-(define-package "flycheck" "20240212.756" "On-the-fly syntax checking"
-  '((emacs "26.1")
-    (pkg-info "0.4")
-    (seq "2.24"))
-  :commit "2b0f85375c8af5b65187cd8aa3d6b286639b2c33" :authors
+(define-package "flycheck" "20240218.2046" "On-the-fly syntax checking"
+  '((emacs "26.1"))
+  :commit "9854769249686cb23c79ec1fff0ac0735a0f7e81" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainers
   '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com"))
@@ -10,7 +8,7 @@
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
   :keywords
   '("convenience" "languages" "tools")
-  :url "http://www.flycheck.org")
+  :url "https://www.flycheck.org")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:

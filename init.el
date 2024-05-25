ed949f9f43cdc220b12b400405fbf17fbabe67f6
김민식 <admin@MacBook-Air.local>
2024-05-25 15:16:21 +0900

    new update MF

--- init.el
+++ init.el
@@ -28,34 +28,10 @@
 (set-face-attribute 'default nil :family "Monaco" :height 120)
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
 (setq inhibit-startup-screen t)
-(setq initial-major-mode 'org-mode)
 (setq initial-scratch-message "")
-(setq ring-bell-function 'ignore)
-(setq auto-save-file-name-transforms
-      `((".*" ,(expand-file-name "~/.emacs.d/auto-save-list/") t)))
+(setq initial-major-mode 'org-mode)
 (setq-default buffer-file-coding-system 'utf-8-unix)
 
-(require 'mouse)
-(xterm-mouse-mode t)
-(defun handle-mouse-event (event)
-  (interactive "e")
-  (let ((scroll-amount 3)
-        (event-type (event-basic-type event)))
-    (cond ((eq event-type 'mouse-5)
-           (scroll-up scroll-amount))
-          ((eq event-type 'mouse-4)
-           (scroll-down scroll-amount))
-          (t (mouse-set-point event)))))
-
-(defun my-eshell-mode-hook ()
-  (display-line-numbers-mode -1))
-(add-hook 'eshell-mode-hook 'my-eshell-mode-hook)
-(add-hook 'shell-mode-hook (lambda () (display-line-numbers-mode 0)))
-(add-hook 'org-mode-hook (lambda () (display-line-numbers-mode 0)))
-
-(global-display-line-numbers-mode t)
-(global-set-key (kbd "<mouse-4>") 'scroll-up-line)
-(global-set-key (kbd "<mouse-5>") 'scroll-down-line)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
 (custom-set-variables
@@ -63,7 +39,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages '(auctex company flycheck helm magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
